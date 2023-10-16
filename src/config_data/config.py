6fba2256df5313136cc93959f7e103d00e06b3c8
EldarAkhmetshin <105647896+EldarAkhmetshin@users.noreply.github.com>
2023-10-16 10:57:00 +0300

    update config.py

--- src/config_data/config.py
+++ src/config_data/config.py
@@ -6,5 +6,5 @@ if not find_dotenv():
     exit('Environment variables not loaded because file .env is missing')
 else:
     load_dotenv()
-    USER_GUEST_PASSWORD = environ.get('USER_GUEST_PASSWORD')
     passwords = loads(environ.get('PASSWORDS'))
+    users = loads(environ.get('USERS'))
