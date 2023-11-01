392c5afbba4f29e15cba705aa05220cad88e4606
EldarAkhmetshin <105647896+EldarAkhmetshin@users.noreply.github.com>
2023-11-01 13:33:49 +0300

    update reference_information_funcs.py

--- src/utils/gui/reference_information_funcs.py
+++ src/utils/gui/reference_information_funcs.py
@@ -40,6 +40,9 @@ class ReferenceInfo(tkinter.Toplevel):
         self.image_logo = Image.open(os.path.abspath(os.path.join('pics', 'company_logo.png'))) \
             .resize((150, 70))
         self.image_logo_pil = ImageTk.PhotoImage(self.image_logo)
+        self.back_icon = Image.open(os.path.abspath(os.path.join('pics', 'back.png'))) \
+            .resize((20, 20))
+        self.back_icon_pil = ImageTk.PhotoImage(self.back_icon)
         super().__init__()
         self.init_gui()
 
@@ -58,19 +61,29 @@ class ReferenceInfo(tkinter.Toplevel):
         self.frame_bottom.pack(fill=BOTH, expand=True)
 
     def show_instruction(self, instruction: str):
+        """
+        Функция для отображения инструкции в HTML виде
+        :param instruction: Инструкция для пользователя обёрнутая в HTML разметку
+        """
         self.frame_header.pack_forget()
         self.frame_body.pack_forget()
         self.init_gui()
         HTMLLabel(self.frame_body, html=instruction).pack(side=TOP, padx=5, pady=5)
+        ttk.Button(self.frame_bottom, image=self.back_icon_pil,
+                                 command=self.render_widgets()).pack(side=LEFT, padx=5, pady=2)
 
     def render_widgets(self):
         """
         Функция рендера всех виджетов окна авторизации пользователя
         """
+        self.frame_header.pack_forget()
+        self.frame_body.pack_forget()
+        self.frame_bottom.pack_forget()
         if self.app_info:
             ttk.Label(self.frame_header, image=self.image_logo_pil).pack(side=TOP, padx=5, pady=5)
             ttk.Label(self.frame_body, text='\nПриложение: ArtPack MoldShop Manadgment'
-                                            '\nВерсия: 1.0.0 от 08.11.23'
+                                            '\nВерсия: 1.0.0 от 08.11.23
+                                            '\nТребования: ОС Windows; 64-bit'
                                             f'\nПользователь: {user_data.get("user_name")}'
                                             f'\nПрава пользователя:'
                                             f'{check_user_rigths()}',
@@ -80,11 +93,11 @@ class ReferenceInfo(tkinter.Toplevel):
             ttk.Button(self.frame_body, text='Взаимодействие со складом',
                        command=lambda: self.show_instruction(f'{instructions.get("warehouse_operations")}'),
                        width=20, style='Regular.TButton').pack(side=TOP, padx=5, pady=5)
-            ttk.Button(self.frame_body, text='Взаимодействие со складом',
-                       command=lambda: self.show_instruction(f'{instructions.get("warehouse_operations")}'),
+            ttk.Button(self.frame_body, text='Перемещение пресс-форм',
+                       command=lambda: self.show_instruction(f'{instructions.get("molds_moving")}'),
                        width=20, style='Regular.TButton').pack(side=TOP, padx=5, pady=5)
-            ttk.Button(self.frame_body, text='Взаимодействие со складом',
-                       command=lambda: self.show_instruction(f'{instructions.get("warehouse_operations")}'),
+            ttk.Button(self.frame_body, text='Загрузка нового BOM',
+                       command=lambda: self.show_instruction(f'{instructions.get("new_bom_uploading")}'),
                        width=20, style='Regular.TButton').pack(side=TOP, padx=5, pady=5)
 
         get_info_log(user='-', message='Reference info widgets were rendered', func_name=self.render_widgets.__name__,
